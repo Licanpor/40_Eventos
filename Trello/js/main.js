573fa1ab4b2caeeac3162bf22e9a16421a88588f
LilianaCanPor <liliana.canpor@gmail.com>
2016-10-26 15:16:34 -0500

    comentarios agregados

--- Trello/js/main.js
+++ Trello/js/main.js
@@ -1,68 +1,62 @@
-var btnAgregar = document.getElementById('btnAgregar');
-var paLista = document.getElementById('paLista');
-var divPrincipal = document.getElementById('divPrincipal');
+var btnAgregar = document.getElementById('btnAgregar');//Boton a;adir lista
+var paLista = document.getElementById('paLista');//Div del form-grop
+var divPrincipal = document.getElementById('divPrincipal');//Div que anida todo.
 
 var añadirLista = function (){
-
-
 	
-	var input = document.createElement('input');
-		input.type = 'text';
-		input.id = 'input'
-		input.className = 'form-control';
-		input.setAttribute('placeholder','Nombre de la lista');
-
-
+	var input = document.createElement('input');//crea elemento input
+		input.type = 'text';//con un atributo type="text"
+		input.id = 'input'//y un id="input"
+		input.className = 'form-control';// y una class="form-control"
+		input.setAttribute('placeholder','Nombre de la lista');//otro que sea un plaholder="Nombre de la lista"
 	
-	var botonLi = document.createElement('button');
-		botonLi.type = 'button';
+	var botonLi = document.createElement('button');//crea elemento button
+		botonLi.type = 'button';//con estos atributos
 		botonLi.id = 'botonLi';
 		botonLi.className = 'btn btn-default mg ';
-		botonLi.innerHTML = 'Listo!';
+		botonLi.innerHTML = 'Listo!';// y un texto.
 
-	paLista.appendChild(input);
-	paLista.appendChild(botonLi);
-	paLista.replaceChild(input, btnAgregar);
+	paLista.appendChild(input);//incrusta el input dentro del form-group
+	paLista.appendChild(botonLi);//incrusta el button dentro del form-group
+	paLista.replaceChild(input, btnAgregar);//remplaza el boton A;adir Lista por el inpu dentro del form-group
 
 	botonLi.addEventListener('click', function(){});
-
+//cuando den click en el boton Listo! ejecutara la funcion a;adirLista declarada arriba.
 
 	var crearTarjeta = function(){
-
 
 		if(input.value==""){
 			input.setAttribute('placeholder', 'Espera! Cuál es el título?');
 		
 			return false;
-		}
+		}//condicion para validar que el campo esta lleno.
 
-		paLista.replaceChild(btnAgregar, botonLi);
-		paLista.removeChild(input);
+		paLista.replaceChild(btnAgregar, botonLi);//remplaza el boton Listo! por el boton A;adir Lista en el form-group.
+		paLista.removeChild(input);//elimina el input.
 
-		var divLista = document.createElement('div');
-			divLista.id = 'paTarjeta';
-		var h4 = document.createElement('h4');
-			h4.id = 'h4';
-		var titulo = document.createTextNode(input.value);
+		var divLista = document.createElement('div');//crea un div correspondiente a la Lista
+			divLista.id = 'paTarjeta';//con un id="paTarjeta"(caja gris)
+		var h4 = document.createElement('h4');//crea un h4
+			h4.id = 'h4';//con este id="h4"
+		var titulo = document.createTextNode(input.value);//hace del valor del input un nodo de texto
 
-		var divForm = document.createElement('div');
-			divForm.className = 'form-group';
-		var textarea = document.createElement('textarea');
-			textarea.className = 'form-control';
-		var botonTar = document.createElement('button');
-			botonTar.id = 'botonTar';
+		var divForm = document.createElement('div');//crea un div correspondiente a un form-group
+			divForm.className = 'form-group';//con esta clase="form-group"
+		var textarea = document.createElement('textarea');//crea un textarea
+			textarea.className = 'form-control';//con esta clase="form-control"
+		var botonTar = document.createElement('button');//crea un button
+			botonTar.id = 'botonTar';//con estos atributos
 			botonTar.type = 'button';
 			botonTar.className = 'btn btn-success';
-			botonTar.innerHTML = 'Añadir Tarjeta';
-
-		
-
+			botonTar.innerHTML = 'Añadir Tarjeta';// y un texto
+	
 		divPrincipal.insertBefore(divLista, divPrincipal.childNodes[0]);
-		divLista.appendChild(h4);
-		h4.appendChild(titulo);
-		divLista.appendChild(divForm);
-		divForm.appendChild(textarea);
-		divForm.appendChild(botonTar);
+/*El div correspondiente a la Lista insertala en el div que anida todo antes de su nodo hijo primero[0]*/
+		divLista.appendChild(h4);//incrusta el h4 en el divLista
+		h4.appendChild(titulo);//incrusta el valor del titulo en el h4
+		divLista.appendChild(divForm);//incrusta el nuevo form-group en el divLista
+		divForm.appendChild(textarea);//incrusta el textarea en el div form-group
+		divForm.appendChild(botonTar);//incrusta el boton para a;adir la tarjeta en el div form-group
 
 		var añadirTarjeta = function(){
 			if(textarea.value==""){
@@ -70,24 +64,23 @@ var añadirLista = function (){
 		
 			return false;
 			}
-			var divTarjeta = document.createElement('div');
+			var divTarjeta = document.createElement('div');//crea div blanco
 				divTarjeta.id = 'divTarjeta';
-			var h5 = document.createElement('h5');
+			var h5 = document.createElement('h5');//crea h5
 				h5.id = 'h5';
-			var tarjeta = document.createTextNode(textarea.value);
-			var ultimoChild = divLista.childNodes.length - 1;
+			var tarjeta = document.createTextNode(textarea.value);//convierte el valor del textarea en un nodo de texto
+			var ultimoChild = divLista.childNodes.length - 1;//Para acceder al ultimo hijo de divLista utilizamos este metodo y lu guardamos en una variable.
 
-			textarea.value = "";
+			textarea.value = "";//limpia el valor del textarea
 
-			divLista.insertBefore(divTarjeta, divLista.childNodes[ultimoChild]);
-			divTarjeta.appendChild(h5);
-			h5.appendChild(tarjeta);
-
+			divLista.insertBefore(divTarjeta, divLista.childNodes[ultimoChild]);//inserte divTarjeta antes de el ultimo hijo de divLista en divLista.
+			divTarjeta.appendChild(h5);//inserta el h5 en el divTarjeta
+			h5.appendChild(tarjeta);//inserta la el textpo del textarea en el h5
 		}
-		botonTar.addEventListener('click', añadirTarjeta);
+		botonTar.addEventListener('click', añadirTarjeta);//cuando den click en el botonTar ejecutara la funcion a;adir Tarjeta
 	}
-	botonLi.addEventListener('click', crearTarjeta);
+	botonLi.addEventListener('click', crearTarjeta);//cuando den click en el botonLi ejecutara la funcion crear tarjeta
 }
-btnAgregar.addEventListener('click', añadirLista);
+btnAgregar.addEventListener('click', añadirLista);//cuando den click en el botonAgregar ejecutara la funcion a;adir Lista.
 
 
--- Twitter/js/main.js
+++ Twitter/js/main.js
@@ -1,46 +1,53 @@
-
-	//Variables
-	var tarea = document.getElementById('tarea'); //Valor del Textarea
-	var btnAgregar = document.getElementById('btnAgregar'); //Valor del boton
-	var lista = document.getElementById('lista');
-
+//Variables
+var tarea = document.getElementById('tarea'); // Textarea
+var btnAgregar = document.getElementById('btnAgregar'); //boton
+var lista = document.getElementById('lista');//ul
+//FUNCIONes
 var agregarTarea = function (){
 
 	if(tarea.value==""){
 		tarea.setAttribute('placeholder', 'Debes ingresar una tarea');
-		tarea.className = 'error';
+/*En lugar de mostrar un span de validacion incorrecta, mostrara un placeholder en el mismo input
+Siempre y cuando el campo este llenado.*/
 		return false;
 	}
 	console.log("voy agregar tarea");
-	var tareaNueva = document.createElement('ol');
-	var input = document.createElement('input');
-	input.type = 'checkbox';
-	var tareaHecha = document.createElement('i');
-	tareaHecha.className = 'hecho fa fa-trash-o fa-lg';	
-	tareaHecha.setAttribute("aria-hidden", "true");
-	var contenido = document.createTextNode(tarea.value); 
+	var tareaNueva = document.createElement('ol');//crea un elemento ol
+	var input = document.createElement('input');//crea elemento input
+		input.type = 'checkbox';//con un atributo type="checkbox"(.type sustituyo a setAttribute)
+	var tareaHecha = document.createElement('i');//crea elemento i para el icono
+		tareaHecha.className = 'hecho fa fa-trash-o fa-lg';//y les da estas clases(.className)
+		tareaHecha.setAttribute("aria-hidden", "true"); //ademas del atributo aria-hidden="true"(no funciono el .aria-hidden)
+	var contenido = document.createTextNode(tarea.value); //TextNode hace del texto un nodo, para poder manejarlo desde JS. En este caso el texto que el usuario ingrese en el textarea.
 
-	tareaNueva.appendChild(input);
-	tareaNueva.appendChild(contenido);
-	tareaNueva.appendChild(tareaHecha);
+		tareaNueva.appendChild(input);//incrusta el input en el ol
+		tareaNueva.appendChild(contenido);//incrusta el texto del textarea en el ol
+		tareaNueva.appendChild(tareaHecha);//incrusta el i en el ol.
 
+//EVENTOs
 	input.onclick = function(){
+/*Cuando hagan click en el input (osea marquen la casita del checkbox) ejecuta esta funcion:
+Si el input ha sido marcado al elemento que anida el texto del textarea osea el ol ponle un atributo class="tachado"
+(en el css esta esa clase dada con una decoracion para tachar el texto, lista para ser activada) si no ponle un atributo class="" */
 		if(input.checked == true){
 			contenido.parentElement.setAttribute("class","tachado");
 		}else{
 			contenido.parentElement.setAttribute("class","");
 		}
 	};
-		
+/*Cuando hagan click en el i(bote de basura) ejecuta esta funcion:
+al ol agregale un atributo de estilo display = "none" para "borrarlo" en realidad esta escondiendo la tarea por ahi en algun lugar */
 	tareaHecha.addEventListener("click", function(){
 		console.log("click en el bote")
 		tareaNueva.style.display = "none";
 	});
 	
-	tarea.value = "";
-	lista.appendChild(tareaNueva);
+	tarea.value = "";//limpia el valor del textarea.
+	lista.appendChild(tareaNueva);//incrusta el ol en el ul
 };
 	
 btnAgregar.addEventListener('click', agregarTarea);
+//cuando den click en el boton para Agregar Tarea ejecuta la funcion agregarTarea(explicada arriba)
+//FIN *_*
  
 	
